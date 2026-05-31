c8b3c9ab2dd9ad1485c1eec06e06cedaaa0a761a
Arjunaa Seshadri <krooks522@gmail.com>
2026-05-31 16:44:06 +0530

    salary

--- src/lib/company.ts
+++ src/lib/company.ts
@@ -1,8 +1,8 @@
 export const COMPANY = {
-  name: 'SVAAS Inframax Solutions',
-  address: '[Company Address, City, State - PIN]',
-  phone: '[Phone Number]',
-  gst: '[GST Number]',
+  name: 'SVAAS Inframax Solutions OPC Pvt. Ltd.',
+  address: 'E-44, Sector 63, Noida, Uttar Pradesh - 201301, India',
+  phone: '0120-4283667',
+  gst: '09ABAC2596R1ZG',
   logo: '/svaas.png',
   stamp: '/stamp.png',
 };
